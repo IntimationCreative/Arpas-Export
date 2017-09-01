6df861941d5f27ddba488bd319e164d70fb946b3
Paul Spence <paul@Pauls-Mac-mini.local>
2017-09-01 16:40:32 +0100

    swaped load order

--- admin/admin.php
+++ admin/admin.php
@@ -3,8 +3,7 @@
  * Admin Setup
  * @since 1.0
  */
-
-include_once plugin_dir_path( __FILE__ ) . '/classes/class-ieu-admin.php';
 include_once plugin_dir_path( __FILE__ ) . '/classes/class-ieu-listtable.php';
+include_once plugin_dir_path( __FILE__ ) . '/classes/class-ieu-admin.php';
 
 $admin_settings = new IntiExportUsersAdmin(); // start admin
